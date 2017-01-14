367ad7770a4c14279798338a2b6f7871f1e157ed
Antonio <carrasco.acd@gmail.com>
2017-01-14 16:37:47 +0100

    Fix the mess with the databases of each environment

--- test/models/base.js
+++ test/models/base.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 after(function (done) {
-  require('dotenv').load({ path: '.env' });
-  mongoose.connect(process.env.MONGODB_TEST_URI, function(){
+  require('dotenv').load({ path: '.env.test' });
+  mongoose.connect(process.env.MONGODB_URI, function(){
       mongoose.connection.db.dropDatabase();
       done()
   });
