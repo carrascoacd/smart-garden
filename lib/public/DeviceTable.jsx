c68ed4af0ddb80ccd61d7376b027cf61f4557a56
Antonio Carrasco <antonio.carrasco@i4s.com>
2017-04-20 17:31:54 +0200

    Improve UI

--- lib/public/DeviceTable.jsx
+++ lib/public/DeviceTable.jsx
@@ -26,35 +26,39 @@ export default class DeviceTable extends React.Component {
 
   render() {
     return (
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHeaderColumn>Device Name</TableHeaderColumn>
-            <TableHeaderColumn>Created at</TableHeaderColumn>
-            <TableHeaderColumn>Moisture</TableHeaderColumn>
-            <TableHeaderColumn>Voltage</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {
-            this.state.deviceList.map(function(device){
-              return (
-                device.weatherEntries.map(function(entry){
-                  return (
-                    <TableRow>
-                      <TableRowColumn>{device.name}</TableRowColumn>
-                      <TableRowColumn>{entry.createdAt}</TableRowColumn>
-                      <TableRowColumn>{entry.moisture}</TableRowColumn>
-                      <TableRowColumn>{entry.currentVoltage}</TableRowColumn>
-                    </TableRow>
-                  )
-                })
-              )
-            })
-          }
-        </TableBody>
-      </Table>
+      <div>
+        {
+          this.state.deviceList.map(function(device){
+            return (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHeaderColumn>Device: {device.name}</TableHeaderColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableHeaderColumn>Created at</TableHeaderColumn>
+                    <TableHeaderColumn>Moisture</TableHeaderColumn>
+                    <TableHeaderColumn>Voltage</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {
+                    device.weatherEntries.slice(0, 10).map(function(entry){
+                      return (
+                        <TableRow>
+                          <TableRowColumn>{entry.createdAt}</TableRowColumn>
+                          <TableRowColumn>{entry.moisture}</TableRowColumn>
+                          <TableRowColumn>{entry.currentVoltage}</TableRowColumn>
+                        </TableRow>
+                      )
+                    })
+                  }
+                </TableBody>
+              </Table>
+            )
+          })
+        }
+      </div>
     )
   }
 
