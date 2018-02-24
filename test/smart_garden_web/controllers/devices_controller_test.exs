5eb42753e0008c8f749683706a628dba16b44304
Antonio <carrasco.acd@gmail.com>
2018-02-24 21:49:05 +0100

    Refactor device controller test

--- test/smart_garden_web/controllers/devices_controller_test.exs
+++ test/smart_garden_web/controllers/devices_controller_test.exs
@@ -1,21 +1,17 @@
 defmodule SmartGardenWeb.DeviceControllerTest do
   use SmartGardenWeb.ConnCase
 
-  test "index devices", %{conn: conn} do
-    device = SmartGarden.Repo.insert!(%SmartGarden.Device{name: "Arduino"})
+  setup do
+    device = SmartGarden.Repo.insert!(%SmartGarden.Device{name: "Arduino device"})
     changeset = %SmartGarden.WeatherEntry{moisture: 300.0, device: device}
     weather_entry = SmartGarden.Repo.insert!(changeset)
+    {:ok, %{device: device, weather_entry: weather_entry}}
+  end
+
+  test "index devices", %{conn: conn, device: device, weather_entry: weather_entry} do
     conn = get conn, device_path(conn, :index)
     assert json_response(conn, 200) == %{
-      "deviceList" => [%{
-        "name" => "Arduino",
-        "id" => device.id,
-        "weatherEntries" => [%{
-          "moisture" => 300,
-          "currentVoltage" => 0,
-          "createdAt" => NaiveDateTime.to_string weather_entry.inserted_at
-        }]
-      }]
+      "deviceList" => [expected_response(device, weather_entry)]
     }
   end
 
@@ -25,13 +21,13 @@ defmodule SmartGardenWeb.DeviceControllerTest do
     assert json_response(conn, 201)
   end
 
-  test "get current device", %{conn: conn} do
-    changeset = %SmartGarden.Device{name: "Arduino device"}
-    device = SmartGarden.Repo.insert!(changeset)
-    changeset = %SmartGarden.WeatherEntry{moisture: 300.0, device: device}
-    weather_entry = SmartGarden.Repo.insert!(changeset)
+  test "get current device", %{conn: conn, device: device, weather_entry: weather_entry} do
     conn = get conn, current_device_path(conn, :current)
-    assert json_response(conn, 200) == %{
+    assert json_response(conn, 200) == expected_response(device, weather_entry)
+  end
+
+  def expected_response(device, weather_entry) do
+    %{
       "name" => "Arduino device",
       "id" => device.id,
       "weatherEntries" => [%{
