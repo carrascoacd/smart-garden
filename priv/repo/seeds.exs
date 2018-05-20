809a48990a54a0c813931f815fd332c7219b59a5
Antonio <carrasco.acd@gmail.com>
2018-05-20 19:36:26 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias SmartGarden.Device
+alias SmartGarden.Interval
+alias SmartGarden.Repo
+
+device = Repo.insert!(Device.changeset(%Device{}, %{name: "Arduino Device"}))
+Repo.insert!(Interval.changeset(%Interval{},
+  %{device_id: device.id, 
+    value: 1, 
+    name: "polling", 
+    execution_schedule: "* * * * *", 
+    active: true, 
+    action: "polling"}))
+Repo.insert!(Interval.changeset(%Interval{}, 
+  %{device_id: device.id, 
+    value: 1, 
+    name: "irrigation", 
+    execution_schedule: "* * * * *", 
+    active: true, 
+    action: "open-valve"}))
