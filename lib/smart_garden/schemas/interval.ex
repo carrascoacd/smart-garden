192b702ab9bed9c02d2cfb43db6154f5d0a847ae
Antonio <carrasco.acd@gmail.com>
2020-05-14 12:49:49 +0200

    Add polling as valid action

--- lib/smart_garden/schemas/interval.ex
+++ lib/smart_garden/schemas/interval.ex
@@ -27,7 +27,7 @@ defmodule SmartGarden.Interval do
     interval
     |> cast(attrs, [:name, :value, :action, :execution_schedule, :device_id, :active, :force_open])
     |> validate_required([:name, :value, :action, :execution_schedule, :device_id])
-    |> validate_inclusion(:action, ["open-valve", "close-valve", "reset"])
+    |> validate_inclusion(:action, ["open-valve", "close-valve", "reset", "polling"])
   end
 
 end
